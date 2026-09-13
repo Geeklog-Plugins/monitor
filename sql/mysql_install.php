8c482b19843884029fbf87c303698bc9a7a00da0
Ben <hostellerie.org@gmail.com>
2026-09-13 04:28:01 +0200

    Modernize Monitor security table for fresh installs

--- sql/mysql_install.php
+++ sql/mysql_install.php
@@ -2,41 +2,28 @@
 
 /* Reminder: always indent with 4 spaces (no tabs). */
 // +---------------------------------------------------------------------------+
-// | Monitor Plugin 1.1                                                        |
+// | Monitor Plugin                                                            |
 // +---------------------------------------------------------------------------+
 // | Installation SQL                                                          |
 // +---------------------------------------------------------------------------+
-// | Copyright (C) 2014 by the following authors:                              |
-// |                                                                           |
-// | Authors: Ben               - ben AT geeklog DOT fr                        |
-// +---------------------------------------------------------------------------+
-// |                                                                           |
-// | This program is licensed under the terms of the GNU General Public License|
-// | as published by the Free Software Foundation; either version 2            |
-// | of the License, or (at your option) any later version.                    |
-// |                                                                           |
-// | This program is distributed in the hope that it will be useful,           |
-// | but WITHOUT ANY WARRANTY; without even the implied warranty of            |
-// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                      |
-// | See the GNU General Public License for more details.                      |
-// |                                                                           |
-// | You should have received a copy of the GNU General Public License         |
-// | along with this program; if not, write to the Free Software Foundation,   |
-// | Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.           |
-// |                                                                           |
-// +---------------------------------------------------------------------------+
-//
 
-
-//database queries
+/**
+ * Fresh-install schema.
+ *
+ * `monitor_ban` is retained during the 1.4.0 transition for legacy ban data
+ * and short-lived security observations. Monitor no longer treats this table as
+ * a full replacement for the dedicated Ban plugin.
+ */
 
 $_SQL[] = "
-CREATE TABLE " . $_TABLES['monitor_ban'] . " ("
-    . " bantype varchar(40) NOT NULL default '',"
-    . " data varchar(255) NOT NULL default '',"
-    . " created datetime NOT NULL,"
-    . " access int(3) NOT NULL default 0"
-    . ") ENGINE=MyISAM
+CREATE TABLE " . $_TABLES['monitor_ban'] . " (
+    bantype varchar(40) NOT NULL default '',
+    data varchar(255) NOT NULL default '',
+    created datetime NOT NULL,
+    access int(10) unsigned NOT NULL default 0,
+    KEY monitor_ban_lookup (bantype, data(191)),
+    KEY monitor_ban_created (created)
+) ENGINE=InnoDB
 ";
 
 ?>
